39a479fcbbe5e6612aead93cca2eee059a3670a9
Alex Oliynyk <asashayko@gmail.com>
2020-08-07 20:15:03 +0300

    test links

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React from 'react';
+import { Link, Router } from "@reach/router";
 import logo from './logo.svg';
 import './App.css';
+
+function TestLink() {
+  return <div>Test link, hello from test link</div>
+}
+
+function Home() {
+  return <div>Home sweet home!</div>
+}
 
 function App() {
   return (
@@ -10,14 +19,13 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Link to="/">Go home</Link>
+        <Link to="/test-link">Test link</Link>
+
+        <Router>
+          <Home path="/" />
+          <TestLink path="/test-link" />
+        </Router>
       </header>
     </div>
   );
